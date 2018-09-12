69dca1778e4c8bad456b1f72c545f5f4c1a9757f
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-12 01:04:27 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -55,7 +55,7 @@ function displayRepositories() {
     .map(
        r =>
          '<li>' +
-          r.git_url +
+          r.html_url +
          ' - <a href="#" data-url="' +
          urlBase + r.name +
          '" onclick="getCommits(this)">Get Commits</a></li>' +
